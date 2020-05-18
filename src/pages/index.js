72da6ebd3518fd88d81a96de21f39ebd6d4c781a
Konrad <konrad@NYUSH1824LP-MX.lan>
2020-05-18 14:40:18 +0100

    new styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -101,6 +101,79 @@ class IndexPage extends React.Component {
       loading: false, msg: null, streams: [], dialog: false,
       signupResult: ""
     };
+    this.inlineStyles = {
+      root: {
+        flex: "0 0 48%",
+        margin: "1%"
+      },
+      link: {
+        textDecoration: "none!important",
+        color: "#222"
+      },
+      media: {
+        height: 200,
+        paddingTop: '56.25%', // 16:9
+      },
+      expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+          duration: theme.transitions.duration.shortest,
+        }),
+      },
+      button: {
+        background: "linear-gradient(270deg, rgb(255, 83, 83) 1.64%, rgb(255, 83, 83) 1.65%, rgb(255, 83, 196) 96.17%)",
+        borderRadius: "12.2404px",
+        color: "rgb(255, 255, 255)",
+        fontFamily: "Montserrat, sans-serif",
+        padding: "0.5em 1em",
+        fontSize: 18,
+        textTransform: "none"
+      },
+      header: {
+        color: "#fff", 
+        fontSize: "8vh", 
+        lineHeight: "10vh"
+      },
+      subheader: {
+        color: "white", 
+        fontSize: "35px", 
+        lineHeight: "50px"
+      },
+      headerFive: {
+        fontSize: "18px",
+        fontWeight: "600"
+      },
+      link: {
+        fontSize: "18px",
+        fontWeight: "300"
+      },
+      expandOpen: {
+        transform: 'rotate(180deg)',
+      },
+      form: {
+        justifyContent: "center",
+        display: "flex"
+      },
+      textField: {
+        background: "#282831",
+        borderRadius: "20px",
+        outline: "none",
+        border: "0",
+        padding: "10px",
+        marginRight: "-30px",
+        flex: "0 0 40%",
+        minWidth: "400px",
+        transition: "border 0.1s ease-out",
+        color: "#fff",
+        "&:focus": {
+          border: "1px solid rgb(255, 83, 196)",
+        },
+        "&::placeholder": {
+          color: "#F9F8FD"
+        }
+      }
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   
@@ -146,9 +219,9 @@ class IndexPage extends React.Component {
           >
 
             <Grid xs="12" sm="7">
-              <h1 className={classes.header}>Discover the world's top creatives <span style={{color: "rgb(254, 83, 196)"}}>live.</span></h1>
+              <h1 className={this.inlineStyles.header}>Discover the world's top creatives <span style={{color: "rgb(254, 83, 196)"}}>live.</span></h1>
               <p style={{color: "#C4C4C4", fontSize: "18px" }}>Liveroom is a leading platform for streaming original live content of artists.</p>
-              <Button onClick={() => this.setState({dialog: true})} styles={{background: "linear-gradient(270deg, rgb(255, 83, 83) 1.64%, rgb(255, 83, 83) 1.65%, rgb(255, 83, 196) 96.17%)"}} className={classes.button}>Sign up / Log in</Button>
+              <Button onClick={() => this.setState({dialog: true})} style={{background: "linear-gradient(270deg, rgb(255, 83, 83) 1.64%, rgb(255, 83, 83) 1.65%, rgb(255, 83, 196) 96.17%)"}} className={this.inlineStyles.button}>Sign up / Log in</Button>
               </Grid>
             <Grid xs="12" sm="5" className="grid-image"><img src={front1} alt="energetic young male dancing in pink illumination"/></Grid>
 
@@ -162,7 +235,7 @@ class IndexPage extends React.Component {
           >
             <Grid item xs="12" sm="6" className="grid-image"><img style={{width: "100%"}} src={img1} alt="energetic young male dancing in pink illumination"/></Grid>
             <Grid item xs="12" sm="6">
-              <h2 className={classes.subheader}>Watch quality livestreams</h2>
+              <h2 className={this.inlineStyles.subheader}>Watch quality livestreams</h2>
               <p style={{color: "#C4C4C4", fontSize: "18px" }}>Watch your favorite artists live from the safety of your home.</p>
             </Grid>
           </Grid>
@@ -174,7 +247,7 @@ class IndexPage extends React.Component {
             spacing={5}
           >
             <Grid item xs="12" sm="6" >
-              <h2 className={classes.subheader}>Support your favorites</h2>
+              <h2 className={this.inlineStyles.subheader}>Support your favorites</h2>
               <p style={{color: "#C4C4C4", fontSize: "18px" }}>Make donations to support your favorite creatives. <br/>Be a part of their success.</p>
             </Grid>
             <Grid item xs="12" sm="6" className="grid-image"><img style={{width: "100%"}} src={img2} alt="energetic young male dancing in pink illumination"/></Grid>
@@ -188,7 +261,7 @@ class IndexPage extends React.Component {
           >
             <Grid item xs="12" sm="6" className="grid-image"><img style={{width: "100%"}} src={img3} alt="energetic young male dancing in pink illumination"/></Grid>
             <Grid item xs="12" sm="6" >
-              <h2 className={classes.subheader}>Get involved</h2>
+              <h2 className={this.inlineStyles.subheader}>Get involved</h2>
               <p style={{color: "#C4C4C4", fontSize: "18px" }}>Get involved in the creative community of your choice. <br/>Make friends online.</p>
             </Grid>
           </Grid>
@@ -201,11 +274,11 @@ class IndexPage extends React.Component {
             style={{margin: "50px 0"}}
           >
             <Grid item xs="12">
-              <h2 className={classes.subheader} style={{textAlign: "center"}}>Gigs happen here. Become part of our community.</h2>
+              <h2 className={this.inlineStyles.subheader} style={{textAlign: "center"}}>Gigs happen here. Become part of our community.</h2>
               <p style={{color: "#C4C4C4", fontSize: "18px", textAlign: "center" }}>Don’t miss out! Liveroom is coming soon with industry leading performances live. <br/>Sign up for our list. Be first to get notified.</p>
-              <form onSubmit={this.handleSubmit} className={classes.form} autoComplete="off">
-                <input name="email" className={classes.textField} id="email" placeholder="Email" />
-                <Button type="submit" className={classes.button}>
+              <form className="subscribe-form" onSubmit={this.handleSubmit} className={this.inlineStyles.form} autoComplete="off">
+                <input name="email" className={this.inlineStyles.textField} id="email" placeholder="Email" />
+                <Button type="submit" className={this.inlineStyles.button}>
                   {this.state.loading ? "Processing..." : "Subscribe"}
                 </Button>
               </form>
@@ -223,17 +296,17 @@ class IndexPage extends React.Component {
               <Logo/>
             </Grid>
             <Grid item xs="12" md="3" >
-              <h5 className={classes.headerFive}>Pages</h5>
-              <Link to="/" className={classes.link}>For Creatives</Link>
-              <Link to="/" className={classes.link}>Explore</Link>
+              <h5 className={this.inlineStyles.headerFive}>Pages</h5>
+              <Link to="/" className={this.inlineStyles.link}>For Creatives</Link>
+              <Link to="/" className={this.inlineStyles.link}>Explore</Link>
             </Grid>
             <Grid item xs="12" md="3" >
-              <h5 className={classes.headerFive}>Privacy</h5>
-              <Link to="/" className={classes.link}>Imprint</Link>
-              <Link to="/" className={classes.link}>Privacy Policy</Link>
+              <h5 className={this.inlineStyles.headerFive}>Privacy</h5>
+              <Link to="/" className={this.inlineStyles.link}>Imprint</Link>
+              <Link to="/" className={this.inlineStyles.link}>Privacy Policy</Link>
             </Grid>
             <Grid item xs="12" md="3" >
-              <h5 className={classes.headerFive}>Social Media</h5>
+              <h5 className={this.inlineStyles.headerFive}>Social Media</h5>
             </Grid>
           </Grid> */}
          
